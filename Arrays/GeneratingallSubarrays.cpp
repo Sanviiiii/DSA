603fffd5adb548e3a97f0da182ffe28a7923b429
Sandhya M <134798737+Sanviiiii@users.noreply.github.com>
2025-03-04 18:34:54 +0530

    subarray

---
+++ Arrays/GeneratingallSubarrays.cpp
@@ -0,0 +1,24 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void allSubarray(vector<int>& nums) {
+     int  n=nums.size();
+      for(int i=0;i<n;i++){
+          for(int j=i;j<n;j++){
+              for(int k=i;k<=j;k++){
+                  cout<<nums[k]<<" ";
+                 
+              }
+               cout << endl;
+          }
+      }  
+    }
+int main()
+{
+    int N = 5;
+    vector<int> a = {1, 2, 4, 5};
+     allSubarray(a);
+    
+    return 0;
+}
