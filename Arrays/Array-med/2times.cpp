ee3327a270dd6aa0e33991ae96cc60c9fdc376b4
Sandhya M <134798737+Sanviiiii@users.noreply.github.com>
2025-03-11 21:25:46 +0530

    Moore's voting algorithm

--- Arrays/Array-med/2times.cpp
+++ Arrays/Array-med/2times.cpp
@@ -34,7 +34,7 @@ int main()
     return 0;
 }
 
-//optimal
+//Better
 
 int majorityElement(vector<int>& nums) {
     int n=nums.size();
@@ -46,6 +46,32 @@ int majorityElement(vector<int>& nums) {
         if(it.second>(n/2)){
             return it.first;
         }
+    }
+    return -1;
+}
+
+//optimal-Moore's voting algorithm
+int majorityElement(vector<int>& nums) {
+    int n=nums.size();
+    int cnt=0;
+    int el;
+    for(int i=0;i<n;i++){
+        if(cnt==0){
+            el=nums[i];
+            cnt++;
+        }
+        else if(nums[i]==el) cnt++;
+        else cnt--;
+    }
+    int cnt1=0;
+    for(int i=0;i<n;i++){
+       if(nums[i]==el){
+        cnt1++;
+       }
+    }
+    if(cnt1>(n/2))
+    {
+        return el;
     }
     return -1;
 }
