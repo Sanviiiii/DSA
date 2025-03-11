e92e86f1a1b79927839b4c5e7538332019944797
Sandhya M <134798737+Sanviiiii@users.noreply.github.com>
2025-03-11 20:31:42 +0530

    two sum

--- Arrays/Array-med/Two-sum.cpp
+++ Arrays/Array-med/Two-sum.cpp
@@ -53,3 +53,51 @@ int main() {
     
     return 0;
 }
+//better
+/*class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            int n=nums.size();
+          vector<int> result;
+           unordered_map <int,int> mpp;
+           for(int i=0;i<n;i++){
+            int a = nums[i];
+            int more=target -a;
+            if(mpp.find(more)!=mpp.end()){
+                return {mpp[more],i};
+            }
+            mpp[a]=i;
+           }
+           return {-1,-1};
+        }
+    };*/
+
+    //optimal for variant 1
+    // Online C++ compiler to run C++ program online
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+ string twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+         sort(nums.begin(), nums.end());
+      int left=0,right=n-1;
+      while(left<right){
+          int sum=nums[right]+nums[left];
+           if (sum == target) {
+            return "YES";
+        }
+          else if(sum<target) left++;
+          else  right--;
+      }
+    return "No";
+    }
+int main() {
+    vector<int> arr =  {2, 6, 5, 8, 11};
+    int target = 14;
+    
+    string ans = twoSum( arr, target);
+    cout << "This is the answer for variant 1: "<<ans << endl;
+    
+    return 0;
+}
