e7f8ac0cee1434d8f953f5a65f19db2de0de88fc
Sandhya M <134798737+Sanviiiii@users.noreply.github.com>
2025-03-12 22:31:04 +0530

    Rearrange by sign variety 2

--- Arrays/Array-med/RearrangeElementsbysign.cpp
+++ Arrays/Array-med/RearrangeElementsbysign.cpp
@@ -67,3 +67,57 @@ int main()
 
   return 0;
 }
+
+
+//Variety-2 when they positives and negatives are not equal
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> RearrangebySign(vector<int>&A ,int n){
+    vector<int> pos;vector<int> neg;
+    for(int i=0;i<A.size();i++){
+        if(A[i]>0){
+            pos.push_back(A[i]);
+        }
+        else neg.push_back(A[i]);
+    }
+if(pos.size()>neg.size())
+    {
+        for(int i=0;i<neg.size();i++){
+      
+      A[2*i] = pos[i];
+      A[2*i+1] = neg[i];
+  }
+  int index=neg.size()*2;
+   for(int i=neg.size();i<pos.size();i++){
+       A[index]=pos[i];
+       index++;
+   } 
+    }
+    else{
+         for(int i=0;i<pos.size();i++){
+      
+      A[2*i] = pos[i];
+      A[2*i+1] = neg[i];
+  }
+  int index=pos.size()*2;
+   for(int i=pos.size();i<neg.size();i++){
+       A[index]=neg[i];
+       index++;
+   } 
+    }
+  return A;
+}
+
+int main()
+   { int n = 4;
+  vector<int> A {1,2,-4,-5,3,-3,9,8,-9,-7,8,-6};
+
+  vector<int> ans = RearrangebySign(A,n);
+  
+  for (int i = 0; i < ans.size(); i++) {
+    cout << ans[i] << " ";
+  }
+
+  return 0;
+}
+
