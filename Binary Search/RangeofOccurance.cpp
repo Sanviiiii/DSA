d05b97b6ec1ed55c8c5eaf4a3eb08d6651355e06
Sandhya M <134798737+Sanviiiii@users.noreply.github.com>
2025-03-24 13:05:15 +0530

    range of occurances

--- Binary Search/RangeofOccurance.cpp
+++ Binary Search/RangeofOccurance.cpp
@@ -24,3 +24,38 @@ int main() {
     
     return 0;
 }
+
+//optimised solution using Lb and ub
+class Solution {
+    public:
+        int lower_bound_index(const vector<int> &nums,int target){
+        int n=nums.size();
+        int low=0,high=n-1;
+        int ans=n;
+        while(low<=high){
+           int mid=(low+high)/2;
+            if(nums[mid]>=target){
+                ans=mid;
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return ans;
+    }
+    int upper_bound_index(const vector<int> &nums,int target){
+        int n=nums.size();
+        
+        int ans=upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+       
+        return ans;
+    }
+        vector<int> searchRange(vector<int>& nums, int target) {
+           int n=nums.size();
+           int lb=lower_bound_index(nums,target);
+           if(lb==n || nums[lb]!= target) return{-1,-1};
+           else return {lb,upper_bound_index(nums,target)-1};
+        }
+    };
+
+//optimised solution using Binary Search
+
