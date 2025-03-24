69c7a1c73ae30570c9652314f099c417414f937f
Sandhya M <134798737+Sanviiiii@users.noreply.github.com>
2025-03-24 14:35:11 +0530

    Range optimal

--- Binary Search/RangeofOccurance.cpp
+++ Binary Search/RangeofOccurance.cpp
@@ -59,3 +59,47 @@ class Solution {
 
 //optimised solution using Binary Search
 
+class Solution {
+    public:
+        int first(vector<int>& nums, int target){
+            int n=nums.size();
+            int low=0,high=n-1,first=-1;
+            while(low<=high){
+               int mid=((low+high)/2);
+                if(nums[mid]==target){
+                    first=mid;
+                    high=mid-1;
+                }
+                else if(nums[mid]<target){
+                   low=mid+1;
+                }
+                else high=mid-1;
+    
+            }
+        return first;
+        }
+        int last(vector<int>& nums, int target){
+            int n=nums.size();
+            int low=0,high=n-1,last=-1;
+            while(low<=high){
+               int mid=((low+high)/2);
+                if(nums[mid]==target){
+                    last=mid;
+                    low=mid+1;
+                }
+                else if(nums[mid]<target){
+                   low=mid+1;
+                }
+                else high=mid-1;
+    
+            }
+        return last;
+        }
+        vector<int> searchRange(vector<int>& nums, int target) {
+           
+           int ft=first(nums,target);
+           if(ft==-1) return {-1,-1};
+           int lt=last(nums,target);
+           return {ft,lt};
+        }
+    };
