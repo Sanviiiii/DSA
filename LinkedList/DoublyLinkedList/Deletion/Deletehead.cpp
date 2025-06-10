024ffa008af6cc44dfec0e7d9ed07b5c92a88e87
Sandhya M <134798737+Sanviiiii@users.noreply.github.com>
2025-06-10 20:54:08 +0530

    DLL

--- LinkedList/DoublyLinkedList/Deletion/Deletehead.cpp
+++ LinkedList/DoublyLinkedList/Deletion/Deletehead.cpp
@@ -1,44 +1,3 @@
-#include<bits/stdc++.h>
-using namespace std;
-class Node{
-    public:
-    int data;
-    Node* next;
-    Node* back;
-   Node(int data1,Node* next1 ,Node* back1){
-       data=data1;
-      next=next1;
-      back=back1;
-   }
-   Node(int data1){
-       data=data1;
-      next=nullptr;
-      back=nullptr;
-   }
-};
-Node* convertArrtoDll(vector<int> &arr){
-    Node* head = new Node(arr[0]);
-    Node* prev =head;
-    for(int   i=1;i<arr.size();i++){
-        Node* temp = new Node(arr[i],nullptr,prev);
-        prev->next=temp;
-        prev=temp;
-    }
-    return head;
-}
-void print(Node* head){
-    Node* temp=head;
-    while(temp){
-        cout<<temp->data<<" ";
-        temp=temp->next;
-    }
-}
-int main(){
-    vector<int> arr={12,4,3,2};
-    Node* head=convertArrtoDll(arr);
-    print(head);
-}
-/*-----take 2------------
 // Online C++ compiler to run C++ program online
 #include <bits/stdc++.h>
 using namespace std;
@@ -76,9 +35,21 @@ void PrintLl(Node* head){
         head=head->next;
     }
 }
+Node* DeleteHead(Node* head){
+    if(head==nullptr) return head;
+    Node* prev =head;
+    head=head->next;
+    head->back=nullptr;
+    prev->next=nullptr;
+    free(prev);
+    return head;
+}
 int main() {
     vector<int> arr={1,2,3,4,5};
     Node* head=ConvertTo2DLl(arr);
     PrintLl(head);
+    cout<<endl<<"Delete head"<<endl;
+    head=DeleteHead(head);
+    PrintLl(head);
     return 0;
-}*/
+}
